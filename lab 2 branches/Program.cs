11286de9c6060ba7eaeca47ee3bfbc8da6ef2c9e
Lososb <58726460+Lososb@users.noreply.github.com>
2023-12-07 17:01:01 +0300

    first commit

---
+++ lab 2 branches/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("my second repository");
+    }
+}
